ee852d43f0743204329939ce9af917f496d77aea
Angrel09 <Angrel09>
2026-04-16 22:15:06 -0600

    v2

--- src/app.js
+++ src/app.js
@@ -4,12 +4,21 @@ const morgan = require('morgan');
 require('dotenv').config();
 
 const app = express();
+const path = require('path'); // Importa path al inicio
 
 // Importar rutas
 const petsRoutes = require('./routes/pets');
 const itemsRoutes = require('./routes/items');
 const shopRoutes = require('./routes/shop');
 const activitiesRoutes = require('./routes/activities');
+
+// Servir archivos estáticos del frontend
+app.use(express.static(path.join(__dirname, '../pou-frontend/dist')));
+
+// Cualquier ruta que no coincida con la API, sirve el index.html del front
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../pou-frontend/dist', 'index.html'));
+});
 
 // Middlewares globales
 app.use(cors());
